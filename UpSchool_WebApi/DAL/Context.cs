bdaa2786e7b5c2579587e0390ab48e5bbe9d3de0
gozdemogus <gozdemog@gmail.com>
2022-12-26 15:58:38 +0300

    project files added

---
+++ UpSchool_WebApi/DAL/Context.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UpSchool_WebApi.DAL.Entities;
+
+namespace UpSchool_WebApi.DAL
+{
+    public class Context : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=tcp:127.0.0.1,1433;Database=ApiProject;MultipleActiveResultSets=true;User=SA;Password=MyPass@word;");
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
